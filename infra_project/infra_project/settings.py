6901c559cca0b43dfdd51124dce74061313af6ab
Артём Матёров <psevdoslon.ya@yandex.ru>
2023-02-10 16:02:23 +0300

    Test commit #23

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['51.250.8.234']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
